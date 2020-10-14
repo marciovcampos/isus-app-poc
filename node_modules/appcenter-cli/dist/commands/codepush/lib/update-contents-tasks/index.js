0801ddcac8ad57d90a6b2319223e92d8fac19b1f
Márcio Vinícius <marcio.campos@outlook.com>
2020-10-14 15:20:39 -0300

    add node_modeles to gitignore

--- node_modules/appcenter-cli/dist/commands/codepush/lib/update-contents-tasks/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.zip = exports.sign = void 0;
-const sign_1 = require("./sign");
-exports.sign = sign_1.default;
-const zip_1 = require("./zip");
-exports.zip = zip_1.default;

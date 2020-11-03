055675ec1a7d638a24df37198e77ec6c8d8fac92
Márcio Vinícius <marcio.campos@outlook.com>
2020-11-03 11:16:17 -0300

    add pod to gitignore

--- ios/Pods/Target Support Files/Pods-isusapp-isusappTests/Pods-isusapp-isusappTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_isusapp_isusappTests : NSObject
-@end
-@implementation PodsDummy_Pods_isusapp_isusappTests
-@end
--- ios/Pods/Target Support Files/Pods-isusapp-tvOS/Pods-isusapp-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_isusapp_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_isusapp_tvOS
-@end

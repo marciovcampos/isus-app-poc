fcd184bbefb07730a34125be50a2f68dc6f32cd0
Márcio Vinícius <marcio.campos@outlook.com>
2020-11-24 15:38:28 -0300

    add tag botão compartilhe

--- src/routes/conteudoDoDrawer.js
+++ src/routes/conteudoDoDrawer.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
-
 import {
   View,
   Text,
@@ -15,6 +14,7 @@ import {
   DrawerItem
 } from '@react-navigation/drawer';
 import { useNavigation } from '@react-navigation/native';
+import { analyticsData } from '../utils/analytics';
 import ItemDrawer from './itemDrawer';
 import packageJson from '../../package.json';
 import Heart from '../assets/icons/isus_hor.svg';
@@ -83,46 +83,49 @@ function conteudoDoDrawer(props) {
       {/* View é relativa a margem de porcentagem em relação a ultima opção do drawer */}
       {/* Caso adicione um item, a margemTop deve diminuir também */}
       <View style={estilos.itensParteInferior}>
-            <DrawerItem
-              icon={() => <Icon name="information" size={22} color="rgba(0, 0, 0, 0.54)" />}
-              label="Sobre o iSUS"
-              labelStyle={{ fontSize: 15 }}
-              inactiveTintColor="#111"
-              activeTintColor="#111"
-              inactiveBackgroundColor="transparent"
-              activeBackgroundColor="transparent"
-              onPress={() => navigationTermos.navigate('SOBRE')}
-            />
+        <DrawerItem
+          icon={() => <Icon name="information" size={22} color="rgba(0, 0, 0, 0.54)" />}
+          label="Sobre o iSUS"
+          labelStyle={{ fontSize: 15 }}
+          inactiveTintColor="#111"
+          activeTintColor="#111"
+          inactiveBackgroundColor="transparent"
+          activeBackgroundColor="transparent"
+          onPress={() => navigationTermos.navigate('SOBRE')}
+        />
       </View>
       <View style={estilos.itensParteInferior}>
-            <DrawerItem
-              icon={() => <Icon name="clipboard-text" size={22} color="rgba(0, 0, 0, 0.54)" />}
-              label="Termos de Uso"
-              labelStyle={{ fontSize: 15 }}
-              inactiveTintColor="#111"
-              activeTintColor="#111"
-              inactiveBackgroundColor="transparent"
-              activeBackgroundColor="transparent"
-              onPress={() => navigationTermos.navigate('TERMOS_DE_USO')}
-            />
+        <DrawerItem
+          icon={() => <Icon name="clipboard-text" size={22} color="rgba(0, 0, 0, 0.54)" />}
+          label="Termos de Uso"
+          labelStyle={{ fontSize: 15 }}
+          inactiveTintColor="#111"
+          activeTintColor="#111"
+          inactiveBackgroundColor="transparent"
+          activeBackgroundColor="transparent"
+          onPress={() => navigationTermos.navigate('TERMOS_DE_USO')}
+        />
       </View>
       <View style={estilos.itensParteInferior}>
-            <DrawerItem
-              icon={() => <Icon name="share-variant" size={22} color="rgba(0, 0, 0, 0.54)" />}
-              label="Compartilhe o iSUS"
-              labelStyle={{ fontSize: 15 }}
-              inactiveTintColor="#111"
-              activeTintColor="#111"
-              inactiveBackgroundColor="transparent"
-              activeBackgroundColor="transparent"
-              onPress={() => aoCompartilhar()}
-            />
+        <DrawerItem
+          icon={() => <Icon name="share-variant" size={22} color="rgba(0, 0, 0, 0.54)" />}
+          label="Compartilhe o iSUS"
+          labelStyle={{ fontSize: 15 }}
+          inactiveTintColor="#111"
+          activeTintColor="#111"
+          inactiveBackgroundColor="transparent"
+          activeBackgroundColor="transparent"
+          onPress={() => {
+            analyticsData('Compartilhe_o_iSUS', 'Click', 'Home');
+            aoCompartilhar();
+          }}
+        />
       </View>
       <View style={estilos.viewVersao}>
         <Text style={estilos.textoVersao}>
           Versão
           {' '}
-          { versaoSistema }
+          {versaoSistema}
         </Text>
       </View>
     </>
